2de28a180141ffefaddcae1bc6c345b6383c2954
StrangeOne101 <strange.toby@gmail.com>
2016-05-23 21:05:41 +1200

    First commit
    Adds hidden things and a SoonTM on index

---
+++ code.js
@@ -0,0 +1,73 @@
+function btnSelect(element) {
+	$(".radio-inline-btn").each(function() {
+		$(this).removeClass("btn-primary");
+		$(this).addClass("btn-default");
+	});
+	$(element).removeClass("btn-default");
+	$(element).addClass("btn-primary");
+}
+
+function getSubs(elementValue) {
+	if (elementValue == "0") return ["None", "Flight", "Spiritual Projection"];
+	if (elementValue == "1") return ["None", "Sand", "Metal", "Lava"];
+	if (elementValue == "2") return ["None", "Lightning", "Combustion"];
+	if (elementValue == "3") return ["None", "Ice", "Plant", "Blood", "Healing"];
+	if (elementValue == "4") return ["None"];
+	if (elementValue == "5") return ["Yes", "No"];
+	return null;
+	
+}
+
+function getCodeElement(elementValue, subValue) {
+	if (elementValue == "0") return ["Air", "Flight", "Spiritual"][subValue];
+	if (elementValue == "1") return ["Earth", "Sand", "Metal", "Lava"][subValue];
+	if (elementValue == "2") return ["Fire", "Lightning", "Combustion"][subValue];
+	if (elementValue == "3") return ["Water", "Ice", "Plant", "Blood", "Healing"][subValue];
+	if (elementValue == "4") return "Chi";
+	if (elementValue == "5") return "Avatar";
+	return null;
+}
+
+function getSub(elementValue, subValue) {
+	if (elementValue == "0") return ["None", "Flight", "Spiritual Projection"][subValue];
+	if (elementValue == "1") return ["None", "Sand", "Metal", "Lava"][subValue];
+	if (elementValue == "2") return ["None", "Lightning", "Combustion"][subValue];
+	if (elementValue == "3") return ["None", "Ice", "Plant", "Blood", "Healing"][subValue];
+	if (elementValue == "4") return ["None"];
+	if (elementValue == "5") return "None";
+	return "None";
+}
+
+
+$(document).ready(function() {
+	$('.line1').click(function() {
+		alert("Hi");
+		  $('.line2').checked('reset');
+		  
+	});
+		
+	$(".line2").click(function() {
+		  $(".line1").checked('reset');
+		  alert("Hi");
+	});
+	
+	$("#id_element").change(function() {
+		var v = $("#id_element")[0].value;
+		if (v == "5") {
+			$("#id_subelement").html('<option value="0">Yes</option><option value="1">No</option>');
+			$("#id_element_option").html("Require Avatar");
+		} else {
+			var s = "";
+			for (var i = 0; i < getSubs(v).length; i += 1) {
+				s = s + "<option value=\"" + i + "\">" + getSubs(v)[i] + "</option>";
+			}
+			$("#id_subelement").html(s);
+			$("#id_element_option").html("Subelement");
+		}
+	});
+	
+	$('[data-toggle="tooltip"]').tooltip(); 
+});
+
+
+	
