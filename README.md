2f5c030ab7b5782d8a7b1fb27765dcb5b049a3fd
StrangeOne101 <strange.toby@gmail.com>
2024-04-04 00:00:05 +1300

    Pariku Birthday 2024

---
+++ README.md
@@ -0,0 +1,10 @@
+# My HTML Project
+
+This project displays an image of a wrapped gift in the center of a blank, white page. The image moves up and down slightly over time in a sin wave motion and increases in size when hovered over.
+
+## Usage
+
+Simply open the `index.html` file in a web browser to view the webpage.
+```
+
+This file is intentionally left blank.
---
+++ scripts/main.js
@@ -0,0 +1,63 @@
+// No JavaScript code is required for this project.
+
+//Listen for when the element with ID "gift" is clicked
+
+var redirectURL = "https://store.steampowered.com";
+var canRedirect = false;
+
+function ready() {
+    var gift = document.getElementById("gift");
+    var letter = document.getElementById("letter");
+    var scaler = document.getElementById("scaler");
+    var player = document.getElementById("player");
+    var volumeSlider = document.getElementById("volumeSlider");
+    var countdownElement = document.getElementById("countdown");
+    var playButton = document.getElementById("playbutton");
+    gift.addEventListener("click", function() {
+        //Change the text of the element with ID "gift" to "Thank you!"
+        gift.className = "hidden";
+        scaler.className = "hidden";
+        letter.className = "";
+
+        //Play the letter_open.wav sound
+        var audio = new Audio("letter_open.ogg");
+        audio.play();
+
+        setTimeout(function() { //Make the text appear after 10s
+            countdownElement.className = ""; 
+            canRedirect = true;   
+        }, 10000);
+
+    });
+
+    letter.addEventListener("click", function() {
+        if (canRedirect) {
+            countdownElement.innerHTML = "(Redirecting...)";
+            document.location = redirectURL;
+            
+        }
+    });
+
+    volumeSlider.addEventListener("input", () => {
+        player.volume = volumeSlider.value;
+
+        if (player.volume == 0) {
+            player.pause();
+        } else {
+            player.play();
+        }
+      });
+      player.volume = 0.3;
+
+      letter.click();
+
+      document.addEventListener("click", function() {
+        playButton.className = "hidden";
+        player.play();
+      });
+}
+
+//Run function when page is ready
+document.addEventListener("DOMContentLoaded", ready);
+
+
